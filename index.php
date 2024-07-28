cec7345856e0a5c0959cc874c18a063be82b90f6
WilliamRobbers <69828637+WilliamRobbers@users.noreply.github.com>
2024-07-29 11:52:49 +1200

    starting code

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Eclipse POS</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="division" id="left">
+    </div>
+    <div class="division" id="right">
+      <div id="content"></div>
+    </div>
+  </body>
+</html>
