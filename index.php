b4f30ba358fe65c722b3a9916c74f59c4aedec7f
WilliamRobbers <69828637+WilliamRobbers@users.noreply.github.com>
2024-08-15 10:57:54 +1200

    add payment buttons and labels

--- index.php
+++ index.php
@@ -7,7 +7,11 @@
   <body>
     <div id="buttonMatrix"></div>
     <div id="rightDiv">
-      <div id="currentOrder"></div>
+      <div id="currentOrder">
+        <h1 id="orderTotal">TOTAL: $xx</h1>
+        <div id="cash" class="paymentButton"><h1 class="paymentLabel">CASH</h1></div>
+        <div id="eftpos" class="paymentButton"><h1 class="paymentLabel">EFTPOS</h1></div>
+      </div>
     </div>
     <script src="script.js"></script>
   </body>
