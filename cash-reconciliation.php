18dfdf10fcaa6020d21e89dc028268c9e35a6dd4
William Robbers <69828637+WilliamRobbers@users.noreply.github.com>
2024-10-30 23:26:51 +1300

    Start reconciliation screen upgrade

--- cash-reconciliation.php
+++ cash-reconciliation.php
@@ -1,27 +1,30 @@
 <?php
   include "dbconn.php";
 
+  // Select the float that was added for the current date
+  $query = "SELECT * FROM cash_reconciliation WHERE date = CURRENT_DATE()";
+  $result = mysqli_query($conn,$query);
+
+  // Store starting float to variable
+  if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+    $float = $row["start_amount"];
+  }
+
+  // Select all orders that were added on the current day
+  $query = "SELECT * FROM orders WHERE DATE(orderdatetime) = CURRENT_DATE()";
+  $result = mysqli_query($conn,$query);
+
+  $orders_sum = 0;
+
+  // Iteratively calculate sum of all orders
+  if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+      $orders_sum += $row["ordertotal"];
+    }
+  }
+
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Select the float that was added for the current date
-    $query = "SELECT * FROM cash_reconciliation WHERE date = CURRENT_DATE()";
-    $result = mysqli_query($conn,$query);
-
-    if (mysqli_num_rows($result) > 0) {
-      $row = mysqli_fetch_assoc($result);
-      $float = $row["start_amount"];
-    }
-
-    // Select all orders that were added on the current day
-    $query = "SELECT * FROM orders WHERE DATE(orderdatetime) = CURRENT_DATE()";
-    $result = mysqli_query($conn,$query);
-
-    $orders_sum = 0;
-
-    if (mysqli_num_rows($result) > 0) {
-      while ($row = mysqli_fetch_assoc($result)) {
-        $orders_sum += $row["ordertotal"];
-      }
-    }
 
     $drawer_tally = $_POST["end-amount"];
 
@@ -45,6 +48,14 @@
   <body>
     <h1>Cash Drawer Tally</h1>
     <form method="POST">
+      <label for="cash-float">Start of Day Cash Float:</label>
+      <br>
+      <input type="text" id="cash-float" class="noborder" value="$<?php echo $float ?>" readonly>
+      <br>
+      <label for="settlement-total">Total Sales Today:</label>
+      <br>
+      <input type="text" id="settlement-total" class="noborder" value="$<?php echo $orders_sum ?>" readonly>
+      <br>
       <label for="end-amount">Enter cash drawer total: $</label>
       <input type="text" class="noborder" id="end-amount" name="end-amount" placeholder="0.00" step="0.01" oninput="validate()" pattern="^[\d]+(\.[\d])?[\d]?$" style="margin-bottom:20px;" required>
       <br>
