ce522a7ee7031f7e309e148996ddc57779c578a7
William Robbers <69828637+WilliamRobbers@users.noreply.github.com>
2024-11-27 20:13:27 +1300

    tidy comments in main js file

--- scripts/main.js
+++ scripts/main.js
@@ -1,52 +1,54 @@
 let cart = []; // Object array to store cart contents
 let total = 0;
 
-// Add new item to cart
+// Description: Adds a new item to JS cart object
+// Called by: button
+// In file: index.php
+// Event: onclick
 function addToCart(itemId, itemName, itemPrice) {
-  // Find itemId in cart array
-  const itemIndex = cart.findIndex(item => item.itemId === itemId);
-
-  // Make item price 2dp
+  const ITEM_INDEX = cart.findIndex(item => item.itemId === itemId);
   itemPrice = itemPrice.toFixed(2);
 
-  if (itemIndex !== -1) {
-    // Item already in cart, increase quantity
-    cart[itemIndex].itemQuantity++;
+  if (itemIndex !== -1) { // If item exists in cart
+    cart[ITEM_INDEX].itemQuantity++;
   } else {
-    // Item not in cart, add new item
     cart.push({ itemId, itemName, itemPrice, itemQuantity: 1 });
   }
 
   updateCart();
 }
 
-// Remove all items from cart
+// Description: Removes all items from cart
+// Called by: clear-cart-button
+// In file: index.php
+// Event: onclick
 function clearCart() {
-  // Empty the cart and update
   cart = [];
   updateCart();
 }
 
-// Remove specific item from cart
+// Description: Removes a specific item from cart
+// Called by: remove-button
+// In file: main.js
+// Event: onclick
 function removeItem(itemId) {
-  // Find itemId in cart array
-  const itemIndex = cart.findIndex(item => item.itemId === itemId);
-
-  // Remove 1 item from cart array starting at itemIndex (remove item at index)
-  cart.splice(itemIndex, 1);
-
+  const ITEM_INDEX = cart.findIndex(item => item.itemId === itemId);
+  cart.splice(ITEM_INDEX, 1);
   updateCart();
 }
 
-// Update cart display on UI
+// Description: Updates the cart display
+// Called by: multiple
+// In file: main.js
+// Event: function call
 function updateCart() {
-  // Variable declarations
-  const cartElement = document.getElementById('cart');
-  const orderTotalElement = document.getElementById('order-total');
+  // Declarations
+  const CART_ELEMENT = document.getElementById('cart');
+  const ORDER_TOTAL_ELEMENT = document.getElementById('order-total');
   total = 0;
 
   // Always create header row
-  cartElement.innerHTML = `
+  CART_ELEMENT.innerHTML = `
     <div class="row" id="cart-header-row" style="margin-top:10px;">
       <div class="td header" id="clear-cart-button" onclick="clearCart()">X</div>
       <div class="td header" style="width:65%">Item</div>
@@ -57,14 +59,13 @@ function updateCart() {
 
   // For each item in cart array, create new row on UI
   cart.forEach(item => {
-    // Retrieve item details from cart
     let id = item.itemId;
     let name = item.itemName;
     let price = item.itemPrice;
     let qty = item.itemQuantity;
 
     // Append row to cart with item information
-    cartElement.innerHTML += `
+    CART_ELEMENT.innerHTML += `
       <div class="row" id="${id}">
         <div class="td remove-button" onclick='removeItem(${id})'>X</div>
         <div class="td" style="width:65%">${name}</div>
@@ -73,59 +74,52 @@ function updateCart() {
       </div>
     `;
 
-    // Add sum of item price to total
     total += (price * qty);
   });
 
   // Set order total label to calculated total
-  orderTotalElement.textContent = total.toFixed(2);
+  ORDER_TOTAL_ELEMENT.textContent = total.toFixed(2);
 }
 
-// Change quantity with spinbuttons
+// Description: Changes item quantity with spinbuttons
+// Called by: qty input
+// In file: main.js
+// Event: onchange
 function changeQty(itemId, newQty) {
-  // Find itemId in cart array
   const itemIndex = cart.findIndex(item => item.itemId === itemId);
-  // Update quantity in cart
   cart[itemIndex].itemQuantity = newQty;
-
   updateCart();
 }
 
 // Cash button is pressed
 document.getElementById("cash").addEventListener("click", function() {
-  // Show payment iframe
   document.getElementById("payment-container").classList.add("show");
-  const iframe = document.getElementById("payment-frame");
-  // Focus on the cash recieved input to increase efficiency
-  iframe.contentWindow.document.getElementById("cash-received").focus();
-  // Set total to order total
-  iframe.contentWindow.document.getElementById("total").value = total.toFixed(2);
+  const IFRAME = document.getElementById("payment-frame");
+  IFRAME.contentWindow.document.getElementById("cash-received").focus();
+  IFRAME.contentWindow.document.getElementById("total").value = total.toFixed(2);
   // Use hidden input to transfer cart object to the iframe
-  iframe.contentWindow.document.getElementById("cart").value = JSON.stringify(cart);
+  IFRAME.contentWindow.document.getElementById("cart").value = JSON.stringify(cart);
 });
 
 // Reconcile button is pressed
 document.getElementById("reconcile-cash").addEventListener("click", function() {
+  const IFRAME = document.getElementById("cash-reconciliation-frame");
   // Refresh iframe content
-  iframe = document.getElementById("cash-reconciliation-frame");
-  iframe.src += "";
-  // Show iframe
+  IFRAME.src += "";
   document.getElementById("cash-reconciliation-container").classList.add("show");
-  // Focus on the end amount input to increase efficiency
   document.getElementById("cash-reconciliation-frame").contentWindow.document.getElementById("end-amount").focus();
 });
 
-// Create new item button
+// Create new item button is pressed
 document.getElementById("create-new-item").addEventListener("click", function() {
   document.getElementById("create-new-item-container").classList.add("show");
 })
 
-// Remove item button
+// Remove item button is pressed
 document.getElementById("remove-item").addEventListener("click", function() {
   document.getElementById("remove-item-container").classList.add("show");
-  const iframe = document.getElementById("remove-item-frame");
-  // Focus on the item name input to increase efficiency
-  iframe.contentWindow.document.getElementById("item-name").focus();
+  const IFRAME = document.getElementById("remove-item-frame");
+  IFRAME.contentWindow.document.getElementById("item-name").focus();
 })
 
 window.addEventListener('message', function(event) {
